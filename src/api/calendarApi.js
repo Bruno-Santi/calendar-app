5028b6c5b98a3e79fca64c0fc065e2caab1cd95e
Bruno Santimaria <brun0.23@outlook.com>
2023-11-07 18:31:20 -0300

    agregado dist

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import { getEnvVariables } from "../helpers";
-
-const { VITE_API_URL } = getEnvVariables();
 
 const calendarApi = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: "https://calendar-app-dlnf.onrender.com/api",
 });
 
 calendarApi.interceptors.request.use((config) => {
